f285859eda86717e57acfb1039c908809f9537e0
Ciprian Gal <ciprian@jlab.org>
2020-01-13 14:29:53 -0500

    refactor code. add weighting for integrated asymmetries

--- utilities/plotAsym.C
+++ utilities/plotAsym.C
@@ -1,4 +1,3 @@
-
 const double pi = acos(-1);
 const double hplanck = 4.135667696e-15; // eV*s
 const double clight = 299792458; //m/s
@@ -15,112 +14,166 @@ double Gamma = Ebeam / melectron;
 double a = 1 / ( 1 + 4*Gamma*Elaser/melectron);//
 double EgammaMax = 4*a*Elaser*std::pow(Gamma,2);//eV
 
+std::vector<TCanvas*> can;
+TF1 *unpolXsec,*unpolXsecEwght,*al,*rg,*aWxSec,*aWxSecE,*a2WxSec;
+TF2 *at,*atWxSec, *at2WxSec,*atWxSecE;
+  
 void updateConsts();
 double calcUnpolXsection(double*,double*);
+double calcUnpolXsectionE2wght(double*,double*);
+
 double calcAL(double*,double*);
+double calcALxSecWght(double*,double*);
+double calcALxSecEWght(double*,double*);
+double calcAL2xSecWght(double*,double*);
+
 double calcAT(double*,double*);
+double calcATxSecWght(double*,double*);
+double calcATxSecEWght(double*,double*);
+double calcAT2xSecWght(double*,double*);
+
 double rhoAng(double*,double*);
+void drawStuff(int);
 
 void plotAsym(){
-  TF1 *unpolXsec = new TF1("unpolXsec",calcUnpolXsection,0,1,1);
-  TCanvas *c1=new TCanvas("c1","c1");
-  auto frame1 = c1->DrawFrame(0,0,1,1);
+  unpolXsec = new TF1("unpolXsec",calcUnpolXsection,0,1,1);
+  unpolXsecEwght = new TF1("unpolXsecEwght",calcUnpolXsectionE2wght,0,1,1);
+  can.push_back(new TCanvas("c1","c1"));
+  can[0]->Divide(2);
+  can[0]->cd(1);
+  auto frame1 = gPad->DrawFrame(0,0,1,1);
   frame1->GetYaxis()->SetTitle("d#sigma/d#rho [barn]");
   frame1->GetXaxis()->SetTitle("#rho");
-
-  TF1 *al=new TF1("al",calcAL,0,1,1);
-  TCanvas *c2=new TCanvas("c2","longitudinal Azz");
-  auto frame2 = c2->DrawFrame(0,-0.3,1,0.7);
+  can[0]->cd(2);
+  auto frame1a = gPad->DrawFrame(0,0,1,1);
+  frame1a->GetYaxis()->SetTitle("d#sigma/d#rho * #rho [barn]");
+  frame1a->GetXaxis()->SetTitle("#rho");
+  
+  al=new TF1("al",calcAL,0,1,1);
+  can.push_back(new TCanvas("c2","longitudinal Azz"));
+  auto frame2 = can[1]->DrawFrame(0,-0.3,1,0.7);
   frame2->GetYaxis()->SetTitle("A_{long}");
   frame2->GetXaxis()->SetTitle("#rho");
 
-  TF2 *at=new TF2("at",calcAT,0,1,-pi,pi,1);
-  TCanvas *c3=new TCanvas("c3","transverse Azz");
-  c3->Divide(3);
+  at=new TF2("at",calcAT,0,1,-pi,pi,1);
+  can.push_back(new TCanvas("c3","transverse Azz"));
+  can[2]->Divide(3);
 
-  TCanvas *c4=new TCanvas("c4","#rho dependence on angle");
-  auto frame3= c4->DrawFrame(0,1e-10,3.5,1e-2);
-  TF1 *rg = new TF1("rg",rhoAng,0,pi,2);
+  can.push_back(new TCanvas("c4","#rho dependence on angle"));
+  auto frame3= can[3]->DrawFrame(0,1e-10,3.5,1e-2);
+  rg = new TF1("rg",rhoAng,0,pi,2);
 
-  updateConsts();
-  c1->cd();
-  unpolXsec->SetParameter(0,a);
-  unpolXsec->SetLineWidth(2);
-  unpolXsec->DrawCopy("same");
-  c2->cd();
-  al->SetParameter(0,a);
-  al->SetLineWidth(2);
-  al->DrawCopy("same");
-  c3->cd(1);
-  Ebeam = 1e9;
-  updateConsts();
-  at->SetParameter(0,a);
-  at->DrawCopy("COLZ");
-  gPad->SetGridx(1);
-  gPad->SetGridy(1);
-  c4->cd();
-  rg->SetParameter(0,a);
-  rg->SetParameter(1,Gamma);
-  rg->DrawCopy("same");
-
+  can.push_back(new TCanvas("c5","weighted asymetries"));
+  aWxSec = new TF1("aWxSec",calcALxSecWght,0,1,1);
+  a2WxSec = new TF1("a2WxSec",calcAL2xSecWght,0,1,1);
+  aWxSecE = new TF1("aWxSecE",calcALxSecEWght,0,1,1);
+  can[4]->Divide(1,3);
+  can[4]->cd(1);
+  auto fr3 = gPad->DrawFrame(0,-0.1,1,0.35);
+  fr3->SetTitle("x-section weighted asymmetry");
+  fr3->GetXaxis()->SetTitle("#rho");
+  can[4]->cd(2);
+  auto fr4 = gPad->DrawFrame(0,0,1,0.15);
+  fr4->SetTitle("x-section weighted asymmetry^2");
+  fr4->GetXaxis()->SetTitle("#rho");
+  can[4]->cd(3);
+  auto fr5 = gPad->DrawFrame(0,-0.1,1,0.35);
+  fr5->SetTitle("x-section*#rho weighted asymmetry");
+  fr5->GetXaxis()->SetTitle("#rho");
+  
   Ebeam = 5e9;
   updateConsts();
-  c1->cd();
-  unpolXsec->SetParameter(0,a);
-  unpolXsec->SetLineColor(1);
-  unpolXsec->SetLineWidth(2);
-  unpolXsec->DrawCopy("same");
-  c2->cd();
-  al->SetParameter(0,a);
-  al->SetLineColor(1);
-  al->SetLineWidth(2);
-  al->DrawCopy("same");
-  c3->cd(2);
-  at->SetParameter(0,a);
-  at->DrawCopy("COLZ");
-  gPad->SetGridx(1);
-  gPad->SetGridy(1);
-  c4->cd();
-  rg->SetLineColor(1);
-  rg->SetParameter(0,a);
-  rg->SetParameter(1,Gamma);
-  rg->DrawCopy("same");
+  drawStuff(0);
+
+  Ebeam = 12e9;
+  updateConsts();
+  drawStuff(1);
 
   Ebeam = 18e9;
   updateConsts();
-  c1->cd();
+  drawStuff(2);
+
+}
+
+void drawStuff(int i){
+  int color[3]={2,1,4};
+  string tit[3]={"5 GeV","12 GeV","18 GeV"};
+
+  can[0]->cd(1);
   unpolXsec->SetParameter(0,a);
-  unpolXsec->SetLineColor(4);
+  unpolXsec->SetLineColor(color[i]);
   unpolXsec->SetLineWidth(2);
   unpolXsec->DrawCopy("same");
+  cout<<"Unpolarized xSection: "<<unpolXsec->Integral(0,1)<<" barn"<<endl;
+  gPad->SetGridx(1);
+  gPad->SetGridy(1);
+  can[0]->cd(2);
+  unpolXsecEwght->SetParameter(0,a);
+  unpolXsecEwght->SetLineColor(color[i]);
+  unpolXsecEwght->SetLineWidth(2);
+  unpolXsecEwght->DrawCopy("same");
+  cout<<"Unpolarized xSection Ewght: "<<unpolXsecEwght->Integral(0,1)<<" barn"<<endl;
   gPad->SetGridx(1);
   gPad->SetGridy(1);
 
-  c2->cd();
+  can[1]->cd();
   al->SetParameter(0,a);
-  al->SetLineColor(4);
+  al->SetLineColor(color[i]);
   al->SetLineWidth(2);
   al->DrawCopy("same");
   gPad->SetGridx(1);
   gPad->SetGridy(1);
 
-  c3->cd(3);
+  can[4]->cd(1);
+  aWxSec->SetParameter(0,a);
+  aWxSec->SetLineColor(color[i]);
+  aWxSec->SetLineWidth(2);
+  aWxSec->DrawCopy("same");
+  gPad->SetGridx(1);
+  gPad->SetGridy(1);
+  cout<<"Xsec weighted asym: "<<aWxSec->Integral(0,1)
+      <<"\t <A>: "<<aWxSec->Integral(0,1)/unpolXsec->Integral(0,1)
+      <<"\t <A>^2: "<<std::pow(aWxSec->Integral(0,1)/unpolXsec->Integral(0,1),2)<<endl;
+
+  can[4]->cd(2);
+  a2WxSec->SetParameter(0,a);
+  a2WxSec->SetLineColor(color[i]);
+  a2WxSec->SetLineWidth(2);
+  a2WxSec->DrawCopy("same");
+  gPad->SetGridx(1);
+  gPad->SetGridy(1);
+  cout<<"Xsec weighted asym2: "<<a2WxSec->Integral(0,1)
+      <<"\t <A^2>: "<<a2WxSec->Integral(0,1)/unpolXsec->Integral(0,1)<<endl;
+
+  can[4]->cd(3);
+  aWxSecE->SetParameter(0,a);
+  aWxSecE->SetLineColor(color[i]);
+  aWxSecE->SetLineWidth(2);
+  aWxSecE->DrawCopy("same");
+  gPad->SetGridx(1);
+  gPad->SetGridy(1);
+  cout<<"Xsec*E weighted asym: "<<aWxSecE->Integral(0,1)
+      <<"\t <EA>: "<<aWxSecE->Integral(0,1)/unpolXsecEwght->Integral(0,1)
+      <<"\t <EA>^2: "<<std::pow(aWxSecE->Integral(0,1),2)/unpolXsecEwght->Integral(0,1)<<endl;
+
+  
+  can[2]->cd(i+1);
   at->SetParameter(0,a);
+  at->SetTitle(Form("%s;#rho;#phi [rad]",tit[i].c_str()));
   at->DrawCopy("COLZ");
   gPad->SetGridx(1);
   gPad->SetGridy(1);
 
-  c4->cd();
+  can[3]->cd();
   rg->SetParameter(0,a);
   rg->SetParameter(1,Gamma);
-  rg->SetLineColor(4);
+  rg->SetLineColor(color[i]);
   rg->DrawCopy("same");
-  gPad->SetLogy(1);
   gPad->SetGridx(1);
   gPad->SetGridy(1);
-  
-}
+  gPad->SetLogy(1);
 
+}
 void updateConsts(){
   Elaser= hplanck * clight / lambda; //eV
   Gamma = Ebeam / melectron;
@@ -144,6 +197,16 @@ double calcUnpolXsection(double *x, double *par){
   return 2*pi*r0*r0*par[0] * (term1 + 1 + term2)/m2toBarn;
 }
 
+double calcUnpolXsectionE2wght(double *x, double *par){
+
+  double term1 = std::pow(x[0],2) * std::pow(1 - par[0], 2) / ( 1 - x[0] * (1 - par[0]) );
+  double term2 = std::pow( (1 - x[0] * (1+par[0])) / (1 - x[0] * (1-par[0])) , 2);
+
+  return 2*pi*r0*r0*par[0] * (term1 + 1 + term2)/m2toBarn * x[0]*x[0];
+}
+
+/*********************************************************************************************/
+
 //longitudinal asymmetry (Azz) for rho = E_gamma/E^max_gamma
 double calcAL(double *x, double *par){
   double unpolXS = calcUnpolXsection(x,par)*m2toBarn;
@@ -153,11 +216,36 @@ double calcAL(double *x, double *par){
   return 2*pi*r0*r0*par[0]/unpolXS * term1 * term2;
 }
 
+double calcALxSecWght(double *x, double *par){
+  double asym = calcAL(x,par);
+  double xSec = calcUnpolXsection(x,par);
+
+  return asym*xSec;
+}
+
+double calcALxSecEWght(double *x, double *par){
+  double asym = calcAL(x,par);
+  double xSec = calcUnpolXsection(x,par);
+
+  return asym*xSec*x[0];
+}
+
+double calcAL2xSecWght(double *x, double *par){
+  double asym = std::pow(calcAL(x,par),2);
+  double xSec = calcUnpolXsection(x,par);
+
+  return asym*xSec;
+}
+
+/*********************************************************************************************/
+
 //rho dependence on scattered photon angle
 double rhoAng(double *x, double *par){
   
   return 1/(1+par[0]*x[0]*x[0]*par[1]*par[1]);
 }
+
+/*********************************************************************************************/
 
 //transverse asymmetry (Azz) for phi (angle of the outgoing photon wrt e- polarization direction)
 //function of rho and phi
@@ -167,4 +255,26 @@ double calcAT(double *x, double *par){
   double term1 = x[0]*(1-par[0])* std::sqrt(4*x[0]*par[0]*(1-x[0])) / (1-x[0]*(1-par[0]));
 
   return 2*pi*r0*r0*par[0]/unpolXS * cos(x[1]) * term1*100;
+}
+
+
+double calcATxSecWght(double *x, double *par){
+  double asym = calcAT(x,par);
+  double xSec = calcUnpolXsection(x,par);
+
+  return asym*xSec;
+}
+
+double calcATxSecEWght(double *x, double *par){
+  double asym = calcAT(x,par);
+  double xSec = calcUnpolXsection(x,par);
+
+  return asym*xSec*x[0];
+}
+
+double calcAT2xSecWght(double *x, double *par){
+  double asym = std::pow(calcAT(x,par),2);
+  double xSec = calcUnpolXsection(x,par);
+
+  return asym*xSec;
 }
